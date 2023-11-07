a35f3463c4023dedd604bfe603ec2fde7ebce15d
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-11-07 17:40:14 -0600

    feat: Get users from database
    This code retrieves the users from the database. The users will be
    displayed later when the API route is handled.

--- main.go
+++ main.go
@@ -7,6 +7,32 @@ import (
 	"net/http"
 )
 
+type user struct {
+	username   string
+	registered string
+	balance    string
+}
+
+func getUsersFromDB(db *sql.DB) []user {
+	sqlite3.Version()
+
+	rows, err := db.Query("select username, registered, balance from users")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	var users []user
+	for rows.Next() {
+		var username, registered, balance string
+		err = rows.Scan(&username, &registered, &balance)
+		if err != nil {
+			panic(err.Error())
+		}
+		users = append(users, user{username, registered, balance})
+	}
+	return users
+}
+
 func getDB() *sql.DB {
 	db, err := sql.Open("sqlite3", "./database.db")
 	if err != nil {
@@ -32,6 +58,7 @@ func createUsersTable(db *sql.DB) {
 func startServer() {
 	db := getDB()
 	createUsersTable(db)
+	users := getUsersFromDB(db)
 
 	fmt.Println("Listening on port 8080")
 
