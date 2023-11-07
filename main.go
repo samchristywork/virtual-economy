219d5ebf940ec00aa73c4c52ec2b554bbcdb9c22
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-11-07 17:30:16 -0600

    feat: Add basic SQL database handling
    This code just creates a new database called users.db. I think sqlite
    should be sufficient for the scope of this project.

--- main.go
+++ main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"github.com/mattn/go-sqlite3"
 	"net/http"
 )
 
+func getDB() *sql.DB {
+	db, err := sql.Open("sqlite3", "./users.db")
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
+}
+
 func startServer() {
+	db := getDB()
+
 	fmt.Println("Listening on port 8080")
 
 	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("static"))))
