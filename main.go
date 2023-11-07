4b2ef1063fa96d2d077f9a3bb7e8180b5e85138e
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-11-07 17:36:23 -0600

    feat: This code creates a users table
    If the users table doesn't exist, this code creates it. The columns are
    username, registered, and balance. More columns to be added later.

--- main.go
+++ main.go
@@ -8,15 +8,30 @@ import (
 )
 
 func getDB() *sql.DB {
-	db, err := sql.Open("sqlite3", "./users.db")
+	db, err := sql.Open("sqlite3", "./database.db")
 	if err != nil {
 		panic(err.Error())
 	}
 	return db
 }
 
+func createUsersTable(db *sql.DB) {
+	sqlStmt := `
+	create table if not exists users (
+		username text not null primary key,
+		registered text not null,
+		balance text not null
+	);
+	`
+	_, err := db.Exec(sqlStmt)
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
 func startServer() {
 	db := getDB()
+	createUsersTable(db)
 
 	fmt.Println("Listening on port 8080")
 
