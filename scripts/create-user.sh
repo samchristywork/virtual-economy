066fe6f976761d4fd8572f633c991ca88e2a1f87
samchristywork <106397573+samchristywork@users.noreply.github.com>
2025-01-30 17:38:40 -0600

    feat: Add a script for creating new users

---
+++ scripts/create-user.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+NAME=$1
+BALANCE=${2:-1000}
+
+if [[ -z "$NAME" ]]; then
+  echo "Usage: $0 <name> [balance]"
+  exit 1
+fi
+
+curl -s -X POST localhost:8000/users \
+  -H "Content-Type: application/json" \
+  -d "{\"name\":\"$NAME\",\"balance\":$BALANCE}" \
+  | jq .
